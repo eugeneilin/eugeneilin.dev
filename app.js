121a3c8ab4b418d7ba0a0d635e53434518cbceed
Eugene <eugenepianist@gmail.com>
2019-07-12 18:23:32 -0400

    design edits

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Show "loader' for 3 seconds, and then show "intro"
 let myVar;
 
 function myLoader() {
@@ -7,4 +8,21 @@ function myLoader() {
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("intro").style.display = "block";
+}
+
+// Shrink "navbar" when user scrolls down 663px from the top of the document
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 425 || document.documentElement.scrollTop > 425) {
+    document.getElementById("navbar").style.fontSize = "18px";
+    document.getElementById("navbar").style.height = "60px";
+    document.getElementById("navbar").style.lineHeight = "60px";
+    document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 10, 1)";
+  } else {
+    document.getElementById("navbar").style.fontSize = "25px";
+    document.getElementById("navbar").style.lineHeight = "120px";
+    document.getElementById("navbar").style.height = "120px";
+    document.getElementById("navbar").style.backgroundColor = "#0000007a";
+  }
 }
