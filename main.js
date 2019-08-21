c3b706b8b74a39cba48d496af9975191c4876f44
Eugene <eugenepianist@gmail.com>
2019-08-20 22:25:25 -0400

    firebase integration

--- main.js
+++ main.js
@@ -114,3 +114,62 @@ window.onkeyup = function (event) {
     document.getElementById('weather-app-modal').style.display = '.';
   }
 };
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUurcDE43sFoXhuNJFKm4E6vZmn7iwYJU",
+  authDomain: "portfolio-website-ad519.firebaseapp.com",
+  databaseURL: "https://portfolio-website-ad519.firebaseio.com",
+  projectId: "portfolio-website-ad519",
+  storageBucket: "",
+  messagingSenderId: "735375370557",
+  appId: "1:735375370557:web:f3cd6e2593b81d03"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Reference messages collection
+const messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contact-form').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+  e.preventDefault();
+
+  // Get values
+  const name = getInputVal('name');
+  const email = getInputVal('email');
+  const message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, email, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert
+  setTimeout(function () {
+    document.querySelector('.alert').style.display = 'none';
+  }, 3000);
+
+  // Reset form
+  document.getElementById('contact-form').reset();
+}
+
+// Function to get form values
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, message) {
+  const newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email,
+    message: message
+  });
+}
