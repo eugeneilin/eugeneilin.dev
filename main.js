34980f2f64a8954732673bdf3cbfb4804e6c2ae5
Eugene Ilin <eugenepianist@gmail.com>
2019-11-11 23:28:04 -0500

    added mern stack project

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAUurcDE43sFoXhuNJFKm4E6vZmn7iwYJU',
-  authDomain: 'portfolio-website-ad519.firebaseapp.com',
-  databaseURL: 'https://portfolio-website-ad519.firebaseio.com',
-  projectId: 'portfolio-website-ad519',
-  storageBucket: 'gs://portfolio-website-ad519.appspot.com',
-  messagingSenderId: '735375370557',
-  appId: '1:735375370557:web:f3cd6e2593b81d03'
+  apiKey: "AIzaSyAUurcDE43sFoXhuNJFKm4E6vZmn7iwYJU",
+  authDomain: "portfolio-website-ad519.firebaseapp.com",
+  databaseURL: "https://portfolio-website-ad519.firebaseio.com",
+  projectId: "portfolio-website-ad519",
+  storageBucket: "gs://portfolio-website-ad519.appspot.com",
+  messagingSenderId: "735375370557",
+  appId: "1:735375370557:web:f3cd6e2593b81d03"
 };
 
 // Initialize Firebase
@@ -20,10 +20,10 @@ function myLoader() {
 }
 
 function showPage() {
-  document.getElementById('loader').style.display = 'none';
-  document.getElementById('intro').style.display = 'block';
-  document.getElementById('navbar').style.display = 'block';
-  document.getElementById('main').style.display = 'block';
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("intro").style.display = "block";
+  document.getElementById("navbar").style.display = "block";
+  document.getElementById("main").style.display = "block";
 }
 
 // Shrink "navbar" when user scrolls down # pixels from the top of the document
@@ -32,92 +32,108 @@ function shrinkNavbar() {
     document.body.scrollTop > 300 ||
     document.documentElement.scrollTop > 300
   ) {
-    document.getElementById('navbar').style.fontSize = '1em';
-    document.getElementById('navbar').style.height = '60px';
-    document.getElementById('navbar').style.lineHeight = '60px';
-    document.getElementById('line').style.height = '2px';
-    document.getElementById('line').style.top = '-2px';
+    document.getElementById("navbar").style.fontSize = "1em";
+    document.getElementById("navbar").style.height = "60px";
+    document.getElementById("navbar").style.lineHeight = "60px";
+    document.getElementById("line").style.height = "2px";
+    document.getElementById("line").style.top = "-2px";
   } else {
-    document.getElementById('navbar').style.fontSize = '';
-    document.getElementById('navbar').style.lineHeight = '';
-    document.getElementById('navbar').style.height = '';
-    document.getElementById('line').style.height = '';
-    document.getElementById('line').style.top = '';
+    document.getElementById("navbar").style.fontSize = "";
+    document.getElementById("navbar").style.lineHeight = "";
+    document.getElementById("navbar").style.height = "";
+    document.getElementById("line").style.height = "";
+    document.getElementById("line").style.top = "";
   }
 }
 
 // Get each project modal
-const pAModal = document.getElementById('pavel-and-anna-modal');
-const hEModal = document.getElementById('html-email-modal');
-const gHFModal = document.getElementById('github-finder-modal');
+const pAModal = document.getElementById("pavel-and-anna-modal");
+const hEModal = document.getElementById("html-email-modal");
+const gHFModal = document.getElementById("github-finder-modal");
+const cAModal = document.getElementById("mern-stack-app-modal");
 
 // Get each project
-const openPAModal = document.getElementById('open-pa-modal');
-const openHEModal = document.getElementById('open-he-modal');
-const openGHFModal = document.getElementById('open-ww-modal');
+const openPAModal = document.getElementById("open-pa-modal");
+const openHEModal = document.getElementById("open-he-modal");
+const openGHFModal = document.getElementById("open-ww-modal");
+const openCAModal = document.getElementById("open-msa-modal");
 
 // When the user clicks each project, open the modal for each one, prevent
 // scrolling of the body, add padding-right to body
 openPAModal.onclick = function() {
-  pAModal.style.display = 'block';
-  document.body.style.overflowY = 'hidden';
-  document.body.style.paddingRight = '10px';
-  document.getElementById('navigation').style.paddingRight = '10px';
+  pAModal.style.display = "block";
+  document.body.style.overflowY = "hidden";
+  document.body.style.paddingRight = "10px";
+  document.getElementById("navigation").style.paddingRight = "10px";
 };
 
 openHEModal.onclick = function() {
-  hEModal.style.display = 'block';
-  document.body.style.overflowY = 'hidden';
-  document.body.style.paddingRight = '10px';
-  document.getElementById('navigation').style.paddingRight = '10px';
+  hEModal.style.display = "block";
+  document.body.style.overflowY = "hidden";
+  document.body.style.paddingRight = "10px";
+  document.getElementById("navigation").style.paddingRight = "10px";
 };
 
 openGHFModal.onclick = function() {
-  gHFModal.style.display = 'block';
-  document.body.style.overflowY = 'hidden';
-  document.body.style.paddingRight = '10px';
-  document.getElementById('navigation').style.paddingRight = '10px';
+  gHFModal.style.display = "block";
+  document.body.style.overflowY = "hidden";
+  document.body.style.paddingRight = "10px";
+  document.getElementById("navigation").style.paddingRight = "10px";
+};
+
+openCAModal.onclick = function() {
+  cAModal.style.display = "block";
+  document.body.style.overflowY = "hidden";
+  document.body.style.paddingRight = "10px";
+  document.getElementById("navigation").style.paddingRight = "10px";
 };
 
 // close the modal for each project and resume body scrolling
-const closeModal = document.getElementsByClassName('close');
+const closeModal = document.getElementsByClassName("close");
 
 let i;
 
 for (i = 0; i < closeModal.length; i++) {
-  closeModal[i].addEventListener('click', function() {
-    this.parentElement.parentElement.parentElement.style.display = 'none';
-    document.body.style.overflowY = '';
-    document.body.style.paddingRight = '';
-    document.getElementById('navigation').style.paddingRight = '';
+  closeModal[i].addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.style.display = "none";
+    document.body.style.overflowY = "";
+    document.body.style.paddingRight = "";
+    document.getElementById("navigation").style.paddingRight = "";
   });
 }
 
 pAModal.onclick = function() {
-  pAModal.style.display = 'none';
-  document.body.style.overflowY = '';
-  document.body.style.paddingRight = '';
-  document.getElementById('navigation').style.paddingRight = '';
+  pAModal.style.display = "none";
+  document.body.style.overflowY = "";
+  document.body.style.paddingRight = "";
+  document.getElementById("navigation").style.paddingRight = "";
 };
 
 hEModal.onclick = function() {
-  hEModal.style.display = 'none';
-  document.body.style.overflowY = '';
-  document.body.style.paddingRight = '';
-  document.getElementById('navigation').style.paddingRight = '';
+  hEModal.style.display = "none";
+  document.body.style.overflowY = "";
+  document.body.style.paddingRight = "";
+  document.getElementById("navigation").style.paddingRight = "";
 };
 
 gHFModal.onclick = function() {
-  gHFModal.style.display = 'none';
-  document.body.style.overflowY = '';
-  document.body.style.paddingRight = '';
-  document.getElementById('navigation').style.paddingRight = '';
+  gHFModal.style.display = "none";
+  document.body.style.overflowY = "";
+  document.body.style.paddingRight = "";
+  document.getElementById("navigation").style.paddingRight = "";
+};
+
+cAModal.onclick = function() {
+  cAModal.style.display = "none";
+  document.body.style.overflowY = "";
+  document.body.style.paddingRight = "";
+  document.getElementById("navigation").style.paddingRight = "";
 };
 
 // Back to top Button
 function backToTop() {
   document.documentElement.scrollTop = 0;
-  document.getElementById('back-to-top-btn').style.display = '';
+  document.getElementById("back-to-top-btn").style.display = "";
 }
 
 function toggleBtnDisplay() {
@@ -125,9 +141,9 @@ function toggleBtnDisplay() {
     document.body.scrollTop > 560 ||
     document.documentElement.scrollTop > 560
   ) {
-    document.getElementById('back-to-top-btn').style.display = 'block';
+    document.getElementById("back-to-top-btn").style.display = "block";
   } else {
-    document.getElementById('back-to-top-btn').style.display = 'none';
+    document.getElementById("back-to-top-btn").style.display = "none";
   }
 }
 
@@ -140,65 +156,65 @@ window.onscroll = function() {
 // use escape key to hide projects modal
 window.onkeyup = function(event) {
   if ((event.key = 27)) {
-    document.getElementById('pavel-and-anna-modal').style.display = 'none';
-    document.getElementById('html-email-modal').style.display = 'none';
-    document.getElementById('github-finder-modal').style.display = 'none';
-    document.body.style.overflowY = '';
-    document.body.style.paddingRight = '';
-    document.getElementById('navigation').style.paddingRight = '';
+    document.getElementById("pavel-and-anna-modal").style.display = "none";
+    document.getElementById("html-email-modal").style.display = "none";
+    document.getElementById("github-finder-modal").style.display = "none";
+    document.body.style.overflowY = "";
+    document.body.style.paddingRight = "";
+    document.getElementById("navigation").style.paddingRight = "";
   } else {
-    document.getElementById('pavel-and-anna-modal').style.display = '';
-    document.getElementById('html-email-modal').style.display = '';
-    document.getElementById('github-finder-modal').style.display = '';
+    document.getElementById("pavel-and-anna-modal").style.display = "";
+    document.getElementById("html-email-modal").style.display = "";
+    document.getElementById("github-finder-modal").style.display = "";
   }
 };
 
 // Display different footer text
-const copyright = document.getElementById('copyright');
-const author = document.getElementById('author');
+const copyright = document.getElementById("copyright");
+const author = document.getElementById("author");
 
 function displayAuthor() {
-  if ((copyright.style.display = 'block') && (author.style.display = 'none')) {
-    copyright.style.display = 'none';
-    author.style.display = 'block';
+  if ((copyright.style.display = "block") && (author.style.display = "none")) {
+    copyright.style.display = "none";
+    author.style.display = "block";
   }
 }
 function displayCopyright() {
-  if ((copyright.style.display = 'none') && (author.style.display = 'block')) {
-    copyright.style.display = 'block';
-    author.style.display = 'none';
+  if ((copyright.style.display = "none") && (author.style.display = "block")) {
+    copyright.style.display = "block";
+    author.style.display = "none";
   }
 }
 
 // Firebase
 // Reference messages collection
-const messagesRef = firebase.database().ref('messages');
+const messagesRef = firebase.database().ref("messages");
 
 // Listen for form submit
-document.getElementById('contact-form').addEventListener('submit', submitForm);
+document.getElementById("contact-form").addEventListener("submit", submitForm);
 
 // Submit form
 function submitForm(e) {
   e.preventDefault();
 
   // Get values
-  const name = getInputVal('name');
-  const email = getInputVal('email');
-  const message = getInputVal('message');
+  const name = getInputVal("name");
+  const email = getInputVal("email");
+  const message = getInputVal("message");
 
   // Save message
   saveMessage(name, email, message);
 
   // Show alert
-  document.querySelector('.alert').style.display = 'block';
+  document.querySelector(".alert").style.display = "block";
 
   // Hide alert in 10 seconds
   setTimeout(function() {
-    document.querySelector('.alert').style.display = 'none';
+    document.querySelector(".alert").style.display = "none";
   }, 10000);
 
   // Reset form
-  document.getElementById('contact-form').reset();
+  document.getElementById("contact-form").reset();
 }
 
 // Function to get form values
