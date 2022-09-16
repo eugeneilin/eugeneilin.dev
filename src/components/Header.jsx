1504eb22720551d80d523a09892c9c76958d3975
Eugene Ilin <eugenepianist@gmail.com>
2022-09-16 15:42:16 -0400

    react landing page

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Eugene Ilin</h1>
+      <h2>Front-End Developer</h2>
+      <div className='social'>
+        <a href='https://github.com/eugeneilin' target='_blank'>
+          <i className='fab fa-github fa-2x'></i>
+        </a>
+        <a href='https://www.linkedin.com/in/eugene-ilin/' target='_blank'>
+          <i className='fab fa-linkedin-in fa-2x'></i>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
